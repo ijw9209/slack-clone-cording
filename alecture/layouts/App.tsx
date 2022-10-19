c375f02093538c7e00af6955cb1f4e353e3ef3bc
jae woong <ijw9209@naver.com>
2022-10-19 13:35:06 +0900

    폴더구조 및 react-router

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
+
 
 const App = () => {
-    return (<div>핫리로딩</div>);
+    return (
+        <Switch>   
+            <Redirect exact path="/" to="/login" />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
+    );
 }
 
 export default App;
+
+//pages
+//component
+//layouts
+
